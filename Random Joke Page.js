919c6bf6e3315ca5d24999996f4c7ef09ae82ac9
Keerthana <139778589+keerthanachowdary21@users.noreply.github.com>
2024-07-01 18:42:11 +0530

    Add files via upload

--- Random Joke Page.js
+++ Random Joke Page.js
@@ -2,19 +2,25 @@ let jokeTextEl = document.getElementById("jokeText");
 let jokeBtnEl = document.getElementById("jokeBtn");
 let spinnerEl = document.getElementById("spinner");
 
+
+let option = {
+    method: "GET"
+};
+
 function jokeGet() {
-    let option = {
-        method: "GET"
-    };
     let url = "https://apis.ccbp.in/jokes/random";
     spinnerEl.classList.remove("d-none");
+    jokeTextEl.classList.add("d-none");
     fetch(url, option)
         .then(function(response) {
             return response.json();
         })
         .then(function(jsonData) {
+            let randomJoke = jsonData.value;
+
             spinnerEl.classList.add("d-none");
-            jokeTextEl.textContent = JSON.stringify(jsonData);
+            jokeTextEl.classList.remove("d-none");
+            jokeTextEl.textContent = randomJoke;
         });
 }
 jokeBtnEl.addEventListener("click", jokeGet);
